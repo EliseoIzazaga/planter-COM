fbaaf406dbd6f810700975e9aad302bdbbabd074
iYellAtPuppys <duarteeliseo@ymail.com>
2020-10-07 17:10:25 -0700

    more function for individual field updates

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1331.h>
 #include <SPI.h>
+#include <Adafruit_I2CDevice.h>
 //#include <String.h>
+
 //color definitions for OLED SPI screen SSD1331 
 #define BLACK   0x0000
 #define BLUE    0x001F
@@ -20,25 +22,10 @@
 #define OLEDSDA A4
 #define OLEDSCL A5
 
-//pin used for temp/humidity sensor
-#define DHT11 2
-
-//pin used for photocell
-#define PHOTOCELL A7 
-
-//pin used for soil moisture sensor
-#define SOILMOISTURE A6
-
-//pins used on the 3 color led
-#define LEDGREEN 9
-#define LEDRED 6
-#define LEDBLUE 5
-
 //create display
 Adafruit_SSD1331 screen = Adafruit_SSD1331(OLEDCS, OLEDDC, OLEDSDA, OLEDSCL, OLEDRES);
+
 /*
-
-
 This function will print the headers 
 on the screen
 over the values over the values that
@@ -53,117 +40,89 @@ void setHeaders()
   screen.setCursor(47,0);
   screen.print("Hum");
   screen.setCursor(0,33);
-  
   screen.print("Soil");
   screen.setCursor(47, 33);
   screen.print("Message");
 }
 /*
-bool function that that detects a change in a value to the sensors,
-it is meant to be used in a comparison and makes the determination
-*/
-bool detectChange(int oldSoilReading, int oldHumidityReading, int oldTempReading)
-{
-  int newSoilReading = analogRead(SOILMOISTURE);
-  int oldHumReading = analogRead(DHT11);
-  int oldTempReading;
-
-}
-/*
-This will redraw only the text under the 
-headers (the values that are changing)
-to the same color as the back ground so 
-that only the area that needs to be updated
-on the screen will be redrawn
+This function will send valued to
+the screen and can also be used to 
+clear the data under the headers. 
 */
 template <class T>
-void clear(T colorBackground, T colorText, int eraseTemp, int eraseHum, int eraseSoil, char eraseMsg[])
+void sendValuesToScreen(T colorText, int temp, int hum, int soil, char* message)
 {
-  screen.setTextColor(colorBackground,colorBackground);
-  screen.setCursor(0,45); //TEMP 
-  screen.print(eraseTemp);
-  screen.setCursor(0, 12); //HUM
-  screen.print(eraseHum);
-  screen.setCursor(47,12); // SOIL 
-  screen.print(eraseSoil);
-  screen.setCursor(47,45); // MESSAGE
-  screen.print(eraseMsg);
+  screen.setTextColor(colorText);
+  screen.setCursor(0,12);
+  screen.print(temp);
+  screen.setCursor(47,12);
+  screen.print(hum);
+  screen.setCursor(0,45);
+  screen.print(soil);
+  screen.setCursor(47, 45);
+  screen.print(message);
 }
 /*
-This will update the values under the headers
-on the screen, it calls the 
-void clear(T colorBackground, T colorText , int oldText)
-function clear the text, this checks if there is a change
-in the soil sensor reading and will update the screen if
-there is a change
+will update the specific value under temp
 */
 template <class T>
-void update(T colorBackground, T colorText)
+void changeTempOnScreen(T colorText, int temp)
 {
-  int soilMoistureReadingBefore = analogRead(SOILMOISTURE); // takes soil moisture 
-  int humidityReadingBefore = analogRead(DHT11);
-  int tempReadingBefore = analogRead(DHT11);
-  screen.setTextColor(colorText, colorBackground);
-  screen.setCursor(0,45); //TEMP
-  screen.print(tempReadingBefore);
-  screen.setCursor(0, 12); //HUM
-  screen.print(humidityReadingBefore);
-  screen.setCursor(47,12); //SOIL
-  screen.print(soilMoistureReadingBefore);
-  screen.setCursor(47,45); //MESSAGE
-  screen.print("This is a message");
-
-  //int soilMoistureValue = analogRead(SOILMOISTURE); //compares it
-  if(/*bool function that that detects a change in a value to the sensors */) //need bool function when change is detected in any of the sesors
-  {
-    clear(colorBackground, colorText, oldNumber); //
-    screen.setTextColor(colorText);
-    screen.setCursor(0,45);
-    screen.print(soilMoistureValue);
-    screen.setCursor(0, 12);
-    screen.print(soilMoistureValue);
-    screen.setCursor(47,12);
-    screen.print(soilMoistureValue);
-    screen.setCursor(47,45);
-    screen.print(soilMoistureValue);
-  }
+  screen.setTextColor(colorText);
+  screen.setCursor(0,12);
+  screen.print(temp);
+}
+/*
+will update the specific value under hum
+*/
+template <class T>
+void changeHumOnScreen(T colorText, int hum)
+{
+  screen.setTextColor(colorText);
+  screen.setCursor(47,12);
+  screen.print(hum);
+}
+/*
+will update soil
+*/
+template <class T>
+void changeSoilOnScreen(T colorText, int soil)
+{
+  screen.setTextColor(colorText);
+  screen.setCursor(0,45);
+  screen.print(soil);
+}
+/*
+will update message
+*/
+template <class T>
+void changeMessageOnScreen(T colorText, char* message)
+{
+  screen.setTextColor(colorText);
+  screen.setCursor(47,45);
+  screen.print(message);
 }
 
-/*bool detectChange()
+void setup()
 {
-  int initialSoilReading = analogRead(SOILMOISTURE);
-  int initialHumidityReading = analogRead(DHT11);
-  int intitialTempReading = analogRead(DHT11);
-
-  int secondSoilReading;
-  int secondHumidityReading;
-  int secondTempReading;
-
-  bool ret = false;
-  while(ret == false)
-  {
-    
-  }
-}*/
-
-void setup() 
-{
-  Serial.begin(9600);
   screen.begin();
   screen.setFont();
-  screen.fillScreen(BLACK);
-  screen.setTextColor(WHITE);
+  screen.fillScreen(RED);
+  screen.setTextColor(YELLOW);
   setHeaders();
-  update(BLACK,GREEN);
-
-  //Pinmodes
-  //pinMode(LEDGREEN, OUTPUT);
-  //pinMode(LEDBLUE, OUTPUT);
-  //pinMode(LEDRED, OUTPUT);
-  //pinMode(SOILMOISTURE, INPUT);
 }
-
-void loop() 
+void loop()
 {
-  update(BLACK,GREEN);
+  char str[] = "WARNING!";
+  sendValuesToScreen(GREEN,1,2,3, str);
+  delay(500);
+  //sendValuesToScreen(RED,1,2,3, str);
+  //delay(500);
+  changeTempOnScreen(RED,1);
+  //delay(500);
+  changeTempOnScreen(BLACK, 94);
+  delay(500);
+  changeTempOnScreen(RED, 94);
+  //delay(500);
+
 }
